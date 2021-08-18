b4eb83b9860337bbe700180eafcaef362d47d7fa
Rashad-Muntar <muntartoure@gmail.com>
2021-08-18 21:09:51 +0000

    add book dynamically to redux

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,5 +1,6 @@
-/* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { createAction } from '../actions';
 
 const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
 
@@ -11,22 +12,35 @@ const BooksForm = () => {
   ));
 
   const [book, setBook] = useState({ title: '', category: '' });
-
+  const dispatch = useDispatch();
+  // const newBook = {};
+  let title = '';
+  let category = '';
   const handleChange = (e) => {
     if (e.target.name === 'title') {
-      setBook({ title: e.target.value });
-    } else {
-      setBook({ category: e.target.value });
+      title = e.target.value;
+      setBook((state) => ({ ...state, title }));
     }
+    if (e.target.name === 'category') {
+      category = e.target.value;
+      setBook((state) => ({ ...state, category }));
+    }
+    // setBook(newBook);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(book);
+    dispatch(createAction({ ...book, id: Math.random().toFixed(2) }));
   };
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input name="title" onChange={handleChange} type="text" className="input" />
       <select name="category" onChange={handleChange}>
         {options}
       </select>
-      <button onClick={handleChange} type="submit" className="btn">Add Book</button>
+      <button type="submit" className="btn">Add Book</button>
     </form>
   );
 };
