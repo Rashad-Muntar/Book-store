76d0f86f15d6b19b4f17ead8052d87a0be682130
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-08-19 11:26:06 -0400

    Enhance the redux store

--- src/reducers/filter.js
+++ src/reducers/filter.js
@@ -1,7 +1,7 @@
 const filterReducer = (state = [], action) => {
   switch (action.type) {
     case 'CHANGE_FILTER':
-      return state.filter((book) => book.category === action.book.category);
+      return state.filter((book) => book.category === action.filter);
     default:
       return state;
   }
