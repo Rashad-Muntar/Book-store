996f1af12d816a0af0067503f17e85a59af85b82
Rashad-Muntar <muntartoure@gmail.com>
2021-08-20 14:19:59 +0000

    add book css

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../styles/Book.css';
 
 function Book({ book, removeBook }) {
   return (
+
     <div className="Lesson-Panel">
       <div className="left-title-area">
-        <span className="School-of Text-Style-8">{book.category}</span>
+        <div className="title Text-Style-6">{book.title}</div>
         <div className="title Text-Style-6">{book.title}</div>
         <span className="Suzanne-Collins">Suzanne Collins</span>
-        <div>
+
+        <div className="delete-area">
           <span className="Comments">Comments</span>
           <button className="Remove" type="button" onClick={() => removeBook(book)}>Remove</button>
           <span className="Edit">Edit</span>
@@ -16,21 +19,22 @@ function Book({ book, removeBook }) {
       </div>
 
       <div className="panel-right-side">
+
         <div className="oval-area-wrapper">
           <div className="Oval-2" />
-          <div>
-            <span className="-Percent-Complete">64%</span>
-            <br />
-            <span className="Completed">Completed</span>
 
+          <div className="percentage-area">
+            <span className="-Percent-Complete">64%</span>
+            <span className="">Completed</span>
           </div>
         </div>
 
         <div className="update-area">
-          <span className="Current-Chapter">Current Chapter</span>
-          <span className="Current-Lesson">Chapter 17</span>
-          <span className="Update-progress">Update progress</span>
+          <span className="">Current Chapter</span>
+          <span className="">Chapter 17</span>
+          <span className="">Update progress</span>
         </div>
+
       </div>
     </div>
   );
@@ -39,7 +43,6 @@ function Book({ book, removeBook }) {
 Book.propTypes = {
   book: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
-  category: PropTypes.string.isRequired,
   removeBook: PropTypes.func.isRequired,
 };
 
